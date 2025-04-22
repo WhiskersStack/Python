37e478ffad30cee48f754a15909b73efdca4b67a
Jen <ssystem32@gmail.com>
2025-04-22 14:26:20 +0300

    Add Type Functions example demonstrating variable types and their outputs

---
+++ Type_Functions.py
@@ -0,0 +1,11 @@
+# Type Functions Ex
+
+print("\nType Functions Ex\n")
+
+num = 10
+str = "Hello"
+bool = True
+
+print(f"The value of num is {num}, and the type is {type(num)}")
+print(f"The value of str is {str}, and the type is {type(str)}")
+print(f"The value of bool is {bool}, and the type is {type(bool)}")
