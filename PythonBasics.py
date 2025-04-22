1102b879d546dd0e7be355e8310a42223d9dd3c0
Jen <ssystem32@gmail.com>
2025-04-22 11:43:59 +0300

    Add initial implementation of PythonBasics.py with examples for input and variable references

---
+++ PythonBasics.py
@@ -0,0 +1,13 @@
+# Ex 1
+print("\nEx 1")
+sentence = input("Enter a sentence: ")
+length = len(sentence)
+print(f"The length of the sentence is : {length}")
+
+#Ex 2
+print("\nEx 2")
+print("I choose the vars : Color, Age, and Animal.")
+print("Because it is to understand the references of the variables.")
+color = "Red"
+age = 33
+animal = "Cat"
