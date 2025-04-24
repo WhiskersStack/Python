5bcc8dd0d2571e24a191f27d427582c73dcceffa
Jen <ssystem32@gmail.com>
2025-04-24 13:54:59 +0300

    Refactor student data structure to include subjects and grades for Alice, Bob, and Charlie; add David's entry and update Bob's grades.

--- Data Structures/StudentDataManagement.py
+++ Data Structures/StudentDataManagement.py
@@ -4,5 +4,31 @@
 print("\nWelcome to the Student Data Management!")
 
 students = {
-    "Alice": {"age": 20, "grade": "40"},
+    "Alice": {
+        "age": 20,
+        "subjects": ["Math", "Science", "Sports"],
+        "grades": {40, 50, 0},
+    },
+    "Bob": {
+        "age": 22,
+        "subjects": ["Math", "Science", "Sports"],
+        "grades": {50, 60, 0},
+    },
+    "Charlie": {
+        "age": 23,
+        "subjects": ["Math", "Science", "Sports"],
+        "grades": {60, 70, 0},
+    },
 }
+
+students["David"] = {
+    "age": 24,
+    "subjects": ["Math", "Science", "Sports"],
+    "grades": {70, 80, 0},
+}
+
+students["Bob"]["grades"] = {55, 60, 0}
+del students["Charlie"]["subjects"][0]
+
+print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+print(students)
