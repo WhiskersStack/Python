4b6dfd3b8e0358dbdc4cad34744bad4ba7624eba
Jen <ssystem32@gmail.com>
2025-04-24 18:00:28 +0300

    Update student data management: fix Bob's grades, add John, and calculate averages

--- Data Structures/StudentDataManagement.py
+++ Data Structures/StudentDataManagement.py
@@ -1,7 +1,7 @@
 #  Student Data Management
 
 
-print("\nWelcome to the Student Data Management!")
+print("\nWelcome to the Student Data Management!\n")
 
 students = {
     "Alice": {
@@ -12,12 +12,17 @@ students = {
     "Bob": {
         "age": 22,
         "subjects": ["Math", "Science", "Sports"],
-        "grades": {50, 60, 0},
+        "grades": {50, 60, 10},
     },
     "Charlie": {
         "age": 23,
         "subjects": ["Math", "Science", "Sports"],
         "grades": {60, 70, 0},
+    },
+    "John": {
+        "age": 21,
+        "subjects": ["Math", "Science", "Sports"],
+        "grades": {89, 99, 80},
     },
 }
 
@@ -27,8 +32,22 @@ students["David"] = {
     "grades": {70, 80, 0},
 }
 
-students["Bob"]["grades"] = {55, 60, 0}
+students["Bob"]["grades"] = {55, 60, 5}
 del students["Charlie"]["subjects"][0]
 
-print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
-print(students)
+bob_grades = list(students["Bob"]["grades"])
+
+bob_average = int(sum(bob_grades) / len(bob_grades))
+print(f"Bob's average grade is: {bob_average}")
+
+highest_average = 0
+for student in students.keys():
+    grades = list(students[student]["grades"])
+    average = int(sum(grades) / len(grades))
+    if average > highest_average:
+        highest_average = average
+        student_name = student
+
+# ~~~
+print("\n***\n")
+print(f"{student_name} has the highest average grade of: {highest_average}")
