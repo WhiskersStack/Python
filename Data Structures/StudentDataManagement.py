3dd71158f77197ab0e15216baea2ced2aa2c71b9
Jen <ssystem32@gmail.com>
2025-04-24 12:50:22 +0300

    Remove student entries for Bob and Charlie from Student Data Management

--- Data Structures/StudentDataManagement.py
+++ Data Structures/StudentDataManagement.py
@@ -5,6 +5,4 @@ print("\nWelcome to the Student Data Management!")
 
 students = {
     "Alice": {"age": 20, "grade": "40"},
-    "Bob": {"age": 22, "grade": "50"},
-    "Charlie": {"age": 23, "grade": "60"},
 }
