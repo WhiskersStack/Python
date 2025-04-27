da3c3f854f9b05e3eb40d527dc70e15bac792d35
Jen <ssystem32@gmail.com>
2025-04-27 11:43:59 +0300

    Add initial implementation of Longest Word Finder functionality

---
+++ Functions/WordFinder.py
@@ -0,0 +1,20 @@
+#  Longest Word Finder
+
+print("\nLongest Word Finder\n")
+
+str = "The quick brown fox jumps over the lazy dog"
+
+def longest_word(text):
+    longest_word = ""
+    length = 0
+    for word in text.split():
+        if len(word) > length:
+            length = len(word)
+            longest_word = word
+
+    word_tuple = (longest_word, length)
+    return word_tuple
+
+
+print("Longest word in the string is: ", longest_word(str)[0])
+print("Length of the longest word is: ", longest_word(str)[1])
