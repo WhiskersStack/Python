cb9dde469dc08e4df5295a4b04979df2c4f9ae69
Jen <ssystem32@gmail.com>
2025-04-28 23:05:11 +0300

    Add initial implementation of Rectangle Area calculation

---
+++ Functions/RectangleArea.py
@@ -0,0 +1,14 @@
+# Calculate Rectangle Area
+
+print("\nCalculate Rectangle Area\n")
+
+len = 10
+wid = 5
+print("Length:", len)
+print("Width:", wid)
+
+def calculate_area(length, width):
+    """Calculate the area of a rectangle."""
+    return length * width
+
+print("\nArea:", calculate_area(len, wid))
