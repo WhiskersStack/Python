43f2b5b9bc788520f4df607cc34ba1a1e3976717
Jen <ssystem32@gmail.com>
2025-05-06 15:58:39 +0300

    Implement logic to determine the student with the highest average score and update their status; add new student entry in JSON data.

--- JSON/Ex2.py
+++ JSON/Ex2.py
@@ -4,3 +4,38 @@ import json
 
 print("\n~~~ Exercise 2: Challenging JSON Practice ~~~")
 
+id = 2
+
+with open("students.json", "r") as file:
+    data = json.load(file)
+
+
+# Finding the student with the highest average score
+for student in data["students"]:
+    average_score = sum(student["scores"]) / len(student["scores"])
+    highest_average_score = 0
+    student["status"] = "participant"
+
+    if average_score > highest_average_score:
+        highest_average_score = average_score
+        student_with_highest_average = student["name"]
+
+# Adding the 'Winner' status to the student with the highest average score
+for student in data["students"]:
+    if student["name"] == student_with_highest_average:
+        student["status"] = "Winner"
+        break
+
+data["students"].append(
+    {
+        "id": id + 1,
+        "name": "John Cena",
+        "scores": [4, 5, 3],
+        "status": "participant",
+    }
+)
+
+with open("students.json", "w") as file:
+    json.dump(data, file, indent=4)
+
+print(f"\n{data['students']}")
