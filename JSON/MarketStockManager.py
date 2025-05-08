88c1d549f073ad320d34671aa5461acd53e1d88a
Jen <ssystem32@gmail.com>
2025-05-08 10:29:12 +0300

    Add functionality to display stocks with less than 20 pcs, filter by category, and search by price range in MarketStockManager.py; update stock quantity for Cereal in stock.json

--- JSON/MarketStockManager.py
+++ JSON/MarketStockManager.py
@@ -18,3 +18,45 @@ for stock in data:
     if stock["stock"] > 50:
         print(f"\n{i}. {stock['name']}")
         i += 1
+
+i = 1
+print("\nStocks with less than 20 pcs :")
+for stock in data:
+    if stock["stock"] < 20:
+        print(f"\n{i}. {stock['name']}")
+        i += 1
+
+print("\n****************************************")
+category = input("\nEnter the category you want to search : ").lower()
+
+i = 1
+print("\nStocks in the category :")
+for stock in data:
+    if stock["category"].lower() == category:
+        print(f"\n{i}. {stock['name']}")
+        i += 1
+
+print("\n****************************************")
+min_price = float(input("\nEnter the minimum price : "))
+max_price = float(input("\nEnter the maximum price : "))
+
+
+i = 1
+print("\nStocks in the price range :")
+for stock in data:
+    if float(stock["price"]) >= min_price and float(stock["price"]) <= max_price:
+        print(f"\n{i}. {stock['name']}")
+        i += 1
+
+def find_product_by_name(name):
+    stock_name = name.lower()
+    for stock in data:
+        if stock["name"].lower() == stock_name:
+            return stock
+    return None
+
+print("\n****************************************\n")
+print("Searching for an apple... :")
+print(find_product_by_name("apple"))
+print("\nSearching for a banana... :")
+print(find_product_by_name("banana"))
