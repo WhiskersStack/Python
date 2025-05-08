d9634800d84a93b668e4b40bd44d650e4bc49d78
Jen <ssystem32@gmail.com>
2025-05-08 10:17:19 +0300

    Fix stock printing logic and enhance output formatting in MarketStockManager.py

--- JSON/MarketStockManager.py
+++ JSON/MarketStockManager.py
@@ -5,6 +5,16 @@ print("\n~~~ Market Stock Manager ~~~")
 with open("stock.json", "r") as file:
     data = json.load(file)
 
+print("\nPriting the stocks in the database :")
 
+i = 1
 for stock in data:
-    print(f"\nName : {stock['name']}")
+    print(f"\n{i}. {stock['name']} - {stock['stock']} pcs")
+    i += 1
+
+i= 1
+print("\nStocks with more than 50 pcs :")
+for stock in data:
+    if stock["stock"] > 50:
+        print(f"\n{i}. {stock['name']}")
+        i += 1
