b602c2b8c9852c7ec31468a5e373aa183ce17c5f
Jen <ssystem32@gmail.com>
2025-05-11 18:39:56 +0300

    Refactor Pokémon info retrieval and storage; enhance find_pokemon function and update JSON file with new Pokémon entries

--- APIs/my_pokemons.py
+++ APIs/my_pokemons.py
@@ -8,6 +8,15 @@ def find_pokemon(id):
         for pokemon in data:
             if pokemon["id"] == id:
                 print(f"\nPokémon found: {pokemon['name']}")
-                return pokemon
+                print(f"ID: {pokemon['id']}")
+                print(f"Height: {pokemon['height']} cm")
+                print(f"Weight: {pokemon['weight']} kg")
+                print("Types: ")
+                for t in pokemon["types"]:
+                    print(f" - {t['type']['name']}")
+                print("Abilities: ")
+                for a in pokemon["abilities"]:
+                    print(f" - {a['ability']['name']}")
+                return False
     print("\nPokémon not found")
-    return None
+    return data
--- APIs/pokemon.py
+++ APIs/pokemon.py
@@ -11,9 +11,8 @@ print("\n*** Welcome to the Pokémon API ***")
 
 flag = True
 
-
 # Get the Pokémon list from the API
-def get_basic_pokemon_info(pokemon_id):
+def get_basic_pokemon_info(pokemon_id, my_pokemon_list):
     url = (
         metadata.BASE_URL + metadata.ENDPOINTS["pokemon"] + pokemon_id
     )  # Base URL + Pokémon endpoint + Pokémon ID
@@ -27,39 +26,33 @@ def get_basic_pokemon_info(pokemon_id):
     # Saving the response to a JSON file
     data = response.json()
 
-    print("\nRandom Pokémon Info : \n")
+    print(f"ID : {data['id']}")
+    print(f"Name : {data['name']}")
+    print(f"Height : {data['height']} cm")
+    print(f"Weight : {data['weight']} kg")
+    print("Types : ")
+    for t in data["types"]:
+        print(f" - {t['type']['name']}")
+    print("Abilities : ")
+    for a in data["abilities"]:
+        print(f" - {a['ability']['name']}")
 
-    # If a Pokémon ID is provided, print its details
-    if pokemon_id != "":
-        print(f"id : {data['id']}")
-        print(f"Name : {data['name']}")
-        print(f"Height : {data['height']} cm")
-        print(f"Weight : {data['weight']} kg")
-        print("Types : ")
-        for t in data["types"]:
-            print(f" - {t['type']['name']}")
-        print("Abilities : ")
-        for a in data["abilities"]:
-            print(f" - {a['ability']['name']}")
-    # If no Pokémon ID is provided, print the list of Pokémon
-    else:
-        print(
-            "\nThe following Pokémon list will be saved in a file named 'pokemon_list.json' in the current directory :\n"
-        )
 
-        pokemon_list = []  # List to store Pokémon names
-        i = 1
-        # Loop through the results and print the Pokémon names
-        for pokemon in data["results"]:
-            pokemon_list.append(pokemon["name"])
-            print(f"{i}. {pokemon['name']}")
-            i += 1
+    pokemon_info = {
+        "id": data["id"],
+        "name": data["name"],
+        "height": data["height"],
+        "weight": data["weight"],
+        "types": [t["type"]["name"] for t in data["types"]],
+        "abilities": [a["ability"]["name"] for a in data["abilities"]],
+    }
 
-        # Save the Pokémon list to a JSON file
-        with open("pokemon_list.json", "w") as f:
-            json.dump(pokemon_list, f, indent=4)
+    my_pokemon_list.append(pokemon_info)  # Add the Pokémon info to the list
 
-        print("\nPokémon list saved to 'pokemon_list.json'")
+    # Save the Pokémon info to a JSON file
+    with open("pokemon_list.json", "w") as f:
+        json.dump(my_pokemon_list, f, indent=4)
+    print("\nPokémon info saved to pokemon_list.json")
 
 
 # Interactive loop to ask the user if they want to draw a Pokémon or fetch the list, and continue or exit
@@ -67,7 +60,6 @@ while flag:
     is_draw = (
         input("\n> Do you want to draw a Pokémon? (y/n): ").strip().lower()
     )  # Ask the user if they want to draw a Pokémon
-
 
     # Check if the user input is valid
     if is_draw == "y":  # If the user wants to draw a Pokémon
@@ -80,18 +72,19 @@ while flag:
 
         random_num = random.randint(1, 1000)  # Generate a random Pokémon ID between 1 and 1000
 
-        if_pokemon = my_pokemons.find_pokemon(random_num)  # Call the function to find a Pokémon with the random ID
+        pokemon_list = my_pokemons.find_pokemon(random_num)  # Call the function to find a Pokémon with the random ID
 
-
-
-        if not if_pokemon:  # If the Pokémon is not found
+        if pokemon_list:  # If the Pokémon is not found
             print("\nYou don't have this Pokémon in your list.")
             print(f"\nPokémon ID: {random_num}")
             print("Fetching Pokémon info from the API...")
             time.sleep(2)
 
-            get_basic_pokemon_info(random_num)  # Call the function to get Pokémon info with the random ID
-
+            get_basic_pokemon_info(
+                str(random_num), pokemon_list
+            )  # Call the function to get Pokémon info with the random ID
+        else:  # If the Pokémon is found
+            print("\n✨✨✨ You already have this Pokémon in your list ✨✨✨")
 
     else:  # If the user doesn't want to draw a Pokémon
         print("\nOk goodbye!")
@@ -107,5 +100,5 @@ while flag:
             time.sleep(1)
 
         print("\nDone!⚡")
-        #break  # End of the program
+        # break  # End of the program
         flag = False
